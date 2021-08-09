dbee51aa4c4bb5a2d6219aea0ad7baad93144cbd
APB98 <borja.basolutions@gmail.com>
2021-08-09 01:34:00 -0500

    changes to the resolvers file

--- server/server.js
+++ server/server.js
@@ -5,14 +5,20 @@ const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 
+// Here we import authMiddleware
+const { authMiddleware } = require("./utils/auth");
+
 const db = require("./config/connection");
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // Add context to our server so data from the authMiddleware() function can pass data to our resolver functions
+  context: authMiddleware,
 });
 
 
